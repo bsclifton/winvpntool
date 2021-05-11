c34aa3758599713303b33f41635e73fb7ca7e712
Brian Clifton <brian@clifton.me>
2021-05-11 12:55:04 -0700

    rev2

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 // Windows VPN Proof of Concept
 // Using RAS API
 
+#define DEFAULT_PHONE_BOOK NULL
+
 // https://docs.microsoft.com/en-us/windows/win32/api/ras/nf-ras-rasenumconnectionsa
 int EnumConnections() {
     DWORD dwCb = 0;
@@ -148,6 +150,97 @@ int EnumEntries() {
     return 0;
 }
 
+// https://www.codeproject.com/Tips/479880/GetLastError-as-std-string
+std::string GetError(DWORD error) {
+    LPVOID lpMsgBuf;
+    DWORD bufLen = FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        error,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf,
+        0, NULL);
+    if (bufLen) {
+        LPCSTR lpMsgStr = (LPCSTR)lpMsgBuf;
+        std::string result(lpMsgStr, lpMsgStr + bufLen);
+
+        LocalFree(lpMsgBuf);
+
+        return result;
+    }
+    return std::string();
+}
+
+// https://docs.microsoft.com/en-us/windows/win32/api/ras/nf-ras-rassetcredentialsa
+DWORD SetCredentials(LPCTSTR username, LPCTSTR password, LPCTSTR entry_name) {
+    RASCREDENTIALS credentials;
+
+    ZeroMemory(&credentials, sizeof(RASCREDENTIALS));
+    credentials.dwSize = sizeof(RASCREDENTIALS);
+    credentials.dwMask = RASCM_UserName | RASCM_Password;
+
+    wcscpy_s(credentials.szUserName, 256, username);
+    wcscpy_s(credentials.szPassword, 256, password);
+
+    DWORD result = RasSetCredentials(DEFAULT_PHONE_BOOK, entry_name, &credentials, FALSE);
+    if (result != ERROR_SUCCESS) {
+        switch (result) {
+        case ERROR_CANNOT_FIND_PHONEBOOK_ENTRY:
+            std::cout << "ERROR_CANNOT_FIND_PHONEBOOK_ENTRY\n";
+            break;
+        case ERROR_CANNOT_OPEN_PHONEBOOK:
+            std::cout << "ERROR_CANNOT_OPEN_PHONEBOOK\n";
+            break;
+        case ERROR_INVALID_PARAMETER:
+            std::cout << "ERROR_INVALID_PARAMETER\n";
+            break;
+        case ERROR_ACCESS_DENIED:
+            std::cout << "ERROR_ACCESS_DENIED\n";
+            break;
+        default:
+            if (result > RASBASE && result < RASBASEEND) {
+                std::cout << "Ras error; check RasError.h for code " << result;
+            } else {
+                std::string error = GetError(result);
+                std::cout << "OTHER ERROR: (" << result << ") " << error << "\n";
+            }
+            break;
+        }
+        return result;
+    }
+
+    return ERROR_SUCCESS;
+}
+
+// https://docs.microsoft.com/en-us/windows/win32/api/ras/nf-ras-rassetentrypropertiesa
+DWORD CreateEntry(LPCTSTR entry_name) {
+    RASENTRY entry;
+    ZeroMemory(&entry, sizeof(RASENTRY));
+    entry.dwSize = sizeof(RASENTRY);
+    // TODO: what options needed?
+    // - `Provider` should be `Windows (built-in)
+    // - `Server name or address` needs to be set to hostname
+    // - `VPN type` should be IKEv2
+    // - `Credentials` - are they set here? or separately (ex: with RasSetCredentials)
+    // 
+    entry.dwfOptions = RASEO_RemoteDefaultGateway | RASEO_ModemLights |
+        RASEO_SecureLocalFiles | RASEO_RequireMsEncryptedPw | RASEO_RequireDataEncryption |
+        RASEO_RequireMsCHAP2 | RASEO_ShowDialingProgress;
+
+    // TODO: finish me 
+    RasSetEntryProperties(DEFAULT_PHONE_BOOK, entry_name,
+        &entry, NULL, NULL, NULL
+        // TBD
+    );
+
+
+    SetCredentials(TEXT("bubba"), TEXT("password1!"), entry_name);
+
+    return ERROR_SUCCESS;
+}
+
 int main() {
     std::cout << "[RAS (Remote Access Service) demo]\nSee https://docs.microsoft.com/en-us/windows/win32/rras/remote-access-service-functions for more info\n\n";
     EnumConnections();
@@ -157,16 +250,6 @@ int main() {
 
     std::cout << std::endl;
     EnumEntries();
-	
-    /*
-    RASCONN abc;
-
-    DWORD result = RasCreatePhonebookEntry(HWND_DESKTOP, L"DEMO1");
-    if (result == ERROR_SUCCESS) {
-        std::cout << "success";
-    } else {
-        std::cout << "failure";
-    }*/
 
 	return 0;
 }
